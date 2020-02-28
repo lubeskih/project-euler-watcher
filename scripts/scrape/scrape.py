4023a00ef5576f73292fc70b3ad5fe63fe4a0d47
lubeskih <hristijan.lubeski@gmail.com>
2020-02-29 00:52:17 +0100

    add last scraped label

--- scripts/scrape/scrape.py
+++ scripts/scrape/scrape.py
@@ -1,10 +1,11 @@
 import requests
 import json
 import time
+import datetime
 
 from bs4 import BeautifulSoup
 
-num_of_pages = 14
+num_of_pages = 1
 
 dump = []
 
@@ -19,12 +20,17 @@ def extract(rows):
             json_object["title"] = cells[1].string
             json_object["solves"] = int(cells[2].string)
 
-            dump.append(json_object)
+            dump[1].append(json_object)
         except IndexError as e:
             pass
 
 
 def main():
+    json_object = dict()
+    currentDT = datetime.datetime.now()
+    dump.append(currentDT.strftime("%a, %b %d, %Y at %I:%M %p"))
+    dump.append([])
+
     for page in range(1, num_of_pages + 1):
         page_body = requests.get(f"https://projecteuler.net/archives;page={page}")
         soup = BeautifulSoup(page_body.content, 'html.parser')
